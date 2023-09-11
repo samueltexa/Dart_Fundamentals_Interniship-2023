78b555b3038c90aeca4c53d976bb3d57757c7c76
samueltexa <woolardsamuels@gmail.com>
2023-09-11 13:37:49 +0300

    for loop

---
+++ Basics/Loops/for.dart
@@ -0,0 +1,30 @@
+main(){
+  List <int> mylist = [1,2,3,4,5];
+  // for(int i = 0; i<mylist.length; i++){
+  //   print(mylist[i]);
+  // }
+
+  // for(int i in mylist){
+  //   if(i==2){
+  //     continue;
+  //   }
+  //    print(i);
+  // }
+
+  for(int i in mylist){
+    if(i==2){
+      break;
+    }
+     print(i);
+  }
+
+  // mylist.forEach((i){
+  //   print(i);
+  // });
+
+  // int x = 1;
+  // for(int x = 10; x>=1;x--){
+  //   print(x);
+  // }
+
+}
