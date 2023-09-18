1694e989cf2acae8eed474c7fddf32efbf4c6a56
samueltexa <woolardsamuels@gmail.com>
2023-09-18 22:43:11 +0300

    switch

--- Basics/switch.dart
+++ Basics/switch.dart
@@ -1,20 +1,21 @@
 import "dart:io";
 main(){
-    stdout.write("Enter conditional variabel: ");
-    String? country_code = stdin.readLineSync();
-    switch(country_code){
-      case "UG":
-        print("Uganda");
-        break;
-      case "Tz":
-        print("Tanzania");
-        break;
-      case "RW":
-        print("Rwanda");
-        break;
-      default:
-        print("Unknown code");
-        break;
-    }
+  while(true){
+  stdout.write("Enter conditional variable: ");
+  String? country_code = stdin.readLineSync();
+  switch(country_code){
+    case "UG":
+      print("Uganda");
+      break;
+    case "Tz":
+      print("Tanzania");
+      break;
+    case "RW":
+      print("Rwanda");
+      break;
+    default:
+      print("Unknown code");
+      break;
+  }
 
-}
+}}
